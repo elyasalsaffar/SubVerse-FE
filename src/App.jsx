e83403bcaa741e878a9e2c014c01fcb5c321f93d
elyasalsaffar <elyas.dusty@gmail.com>
2025-07-28 20:07:51 +0300

    Initializing frontend main aspects

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { use, useEffect, useState } from 'react'
 import './App.css'
+import { CheckSession } from './services/Auth'
 
 const App = () => {
+
+  const [user, setUser] = useState(null)
+
+  const location = useLocation()
+
+  const handleLogOut = () => {
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    try {
+      const user = await CheckSession()
+      setUser(user)
+    } catch (error) {
+      console.error('Session check failed: ', error)
+      localStorage.removeItem('token')
+    }
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
 
   return (
     <>
